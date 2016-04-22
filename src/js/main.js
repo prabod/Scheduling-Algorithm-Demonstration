73de5453561195a7bd20b970d7ee71b1eaf3d205
Prabod Rathnayaka <prabod@rathnayaka.me>
2016-04-23 02:28:49 +0530

    SJF

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,88 @@
 $(document).ready(function () {
-    var i = $("#form").size()+1;
+    var i = $("#form").size() + 1;
+    var selected = "fcfs";
+    var interval;
+    $("#sjf").change(
+        function () {
+            if ($(this).is(':checked')) {
+                selected = "sjf";
+                $("#atH").css("display", "inline");
+                $("#form").empty();
+                i = $("#form").size();
+                $("#form").append("" +
+                    "<div class='row' id='process" + i + "'>\
+                        <div class='col-lg-4'>\
+                            <label>P" + i + "</label>\
+                        </div>\
+                        <div class='col-lg-4'>\
+                            <input type='number' min='1' max='10' class='form-control' id='burstTime" + i + "' name='process" + i + "'>\
+                        </div>\
+                        <div class='col-lg-4'>\
+                            <input type='number' min='1' max='50' class='form-control' id='arrivalTime" + i + "' name='at" + i + "'>\
+                        </div>\
+                    </div>"
+                );
+                i++;
+            }
+        });
 
+    $("#fcfs").change(
+        function () {
+            if ($(this).is(':checked')) {
+                selected = "fcfs";
+                $("#atH").css("display", "none");
+                $("#form").empty();
+                i = $("#form").size();
+                $("#form").append("" +
+                    "<div class='row' id='process" + i + "'>\
+                        <div class='col-lg-4'>\
+                            <label>P" + i + "</label>\
+                        </div>\
+                        <div class='col-lg-4'>\
+                            <input type='number' min='1' max='10' class='form-control' id='burstTime" + i + "' name='process" + i + "'>\
+                        </div>\
+                        <div class='col-lg-4'>\
+                        </div>\
+                    </div>"
+                );
+                i++;
+            }
+        });
 
     $("#add").click(function () {
         if (i <= 5) {
-            $("#form").append("<div class='row' id='process'><div class='col-lg-4'>\
-            <label>P" + i + "</label>\
-            </div>\
-            <div class='col-lg-4'>\
-            <input type='number' min='1' max='10' class='form-control' id='burstTime" + i + "' name='process" + i + "'>\
-            </div>\
-            <div>\
-        </div>\
-        </div>"
-            );
+            if (selected === "fcfs") {
+                $("#form").append("" +
+                    "<div class='row' id='process" + i + "'>\
+                        <div class='col-lg-4'>\
+                            <label>P" + i + "</label>\
+                        </div>\
+                        <div class='col-lg-4'>\
+                            <input type='number' min='1' max='10' class='form-control' id='burstTime" + i + "' name='process" + i + "'>\
+                        </div>\
+                        <div class='col-lg-4'>\
+                        </div>\
+                    </div>"
+                );
 
-            i++;
+                i++;
+            }
+            else if (selected === "sjf") {
+                $("#form").append("" +
+                    "<div class='row' id='process" + i + "'>\
+                        <div class='col-lg-4'>\
+                            <label>P" + i + "</label>\
+                        </div>\
+                        <div class='col-lg-4'>\
+                            <input type='number' min='1' max='10' class='form-control' id='burstTime" + i + "' name='process" + i + "'>\
+                        </div>\
+                        <div class='col-lg-4'>\
+                            <input type='number' min='1' max='50' class='form-control' id='arrivalTime" + i + "' name='at" + i + "'>\
+                        </div>\
+                    </div>"
+                );
+                i++;
+            }
         }
     });
 
@@ -23,43 +90,53 @@ $(document).ready(function () {
         e.preventDefault();
         var error = false;
         /*for(var k =1;k<=i;k++){
-            if(!$("burstTime"+k).val()){
-                $("burstTime"+k).addClass('warning');
-                error = true;
-            }
-        }\
-        */
+         if(!$("burstTime"+k).val()){
+         $("burstTime"+k).addClass('warning');
+         error = true;
+         }
+         }\
+         */
         $("#barpanel").empty();
         if (!error) {
-            SJF()
+            var timer = 1;
+            interval = setInterval(function () {
+                $("#timet").text(timer / 60 + "s");
+                timer++
+            }, 1000 / 60);
+            if (selected == "sjf") {
+                SJF();
+            }
+            else if (selected == "fcfs") {
+                FCFS();
+            }
         }
     });
 
     function FCFS() {
         var process = [];
         var rectangles = [];
-        var waitingTime = [0];
         var totalTime = 0;
         for (var j = 1; j < i; j++) {
             var time = $("#burstTime" + j).val();
-            process.push(parseInt(time));
             totalTime += parseInt(time);
             if (j != 1) {
-                var wt = 0;
+                var wti = 0;
                 for (var k = 0; k < j - 1; k++) {
-                    wt += process[k];
+                    wti += process[k].bt;
                 }
-                waitingTime.push(wt);
+                process.push({"name": "P" + j, "bt": parseInt(time), "wt": wti});
+            }
+            else {
+                process.push({"name": "P" + j, "bt": parseInt(time), "wt": 0});
             }
         }
         var scale = Math.round(500 / totalTime);
-        console.log(scale);
-        console.log(waitingTime);
         for (var j = 1; j < i; j++) {
-            $("#barpanel").append("<div class='row' style='height: 30px;margin-top: 5px;width:"+scale * process[j - 1]+"px;margin-left: "+scale * waitingTime[j - 1]+"px' id='bar"+j+"'></div>");
-            console.log(scale * waitingTime[j - 1] +' '+ scale * process[j - 1]);
-            setTimeout(visualize, waitingTime[j - 1] * 1000, '#bar' + j, process[j - 1], j);
+            $("#barpanel").append("<div class='row' style='height: 30px;margin-top: 5px;width:" + scale * process[j - 1].bt + "px;margin-left: " + scale * process[j - 1].wt + "px' id='bar" + j + "'></div>");
+            console.log(scale * process[j - 1].wt + ' ' + scale * process[j - 1].bt);
+            setTimeout(visualize, process[j - 1].wt * 1000, '#bar' + j, process[j - 1]);
         }
+        clearInterval(interval);
     }
 
     function SJF() {
@@ -68,13 +145,18 @@ $(document).ready(function () {
         var elapsedTime = 0;
         for (var j = 1; j < i; j++) {
             var time = $("#burstTime" + j).val();
-            process.push({"name": "P" + j, "bt": parseInt(time), "wt": 0, "at": 0,});
+            var artime = $("#arrivalTime" + j).val();
+            process.push({"name": "P" + j, "bt": parseInt(time), "wt": 0, "at": artime});
             totalTime += parseInt(time);
         }
         var processDup = [];
         var scale = Math.round(500 / totalTime);
         while (process.length > 0) {
-            var j = shortestJob(process);
+            var j = shortestJob(process, elapsedTime);
+            if (j == "no") {
+                elapsedTime++;
+                continue;
+            }
             console.log(j);
             for (var z = 0; z < process.length; z++) {
                 if (z != j - 1) {
@@ -91,24 +173,24 @@ $(document).ready(function () {
         console.log();
     }
 
-    function shortestJob(p) {
+    function shortestJob(p, eTime) {
         var index = 0;
-        var min = p[0].bt;
-        for (var k = 1; k < p.length; k++) {
-            if (p[k].bt < min) {
+        var min = Number.MAX_VALUE;
+        for (var k = 0; k < p.length; k++) {
+            if (p[k].bt < min && p[k].at <= eTime) {
                 min = p[k].bt;
                 index = k;
             }
         }
-        return index + 1;
+        return min == Number.MAX_VALUE ? "no" : index + 1;
     }
 
-    function visualize(container,duration) {
+    function visualize(container, duration) {
         var bar = new ProgressBar.Line(container, {
-            strokeWidth: 4,
-            easing: 'easeInOut',
+                strokeWidth: 4,
+                easing: 'easeInOut',
                 duration: duration.bt * 1000,
-            color: '#FFEA82',
+                color: '#FFEA82',
                 text: {
                     value: duration.name,
                     className: 'progressbar__label',
@@ -129,12 +211,12 @@ $(document).ready(function () {
                     }
 
                 },
-            svgStyle: {width: '100%', height: '100%'},
-            from: {color: '#FFEA82'},
-            to: {color: '#ED6A5A'},
-            step: (state, bar) => {
+                svgStyle: {width: '100%', height: '100%'},
+                from: {color: '#FFEA82'},
+                to: {color: '#ED6A5A'},
+                step: (state, bar) = > {
                 bar.path.setAttribute('stroke', state.color);
-            }
+    }
     })
         bar.animate(1.0);  // Number from 0.0 to 1.0
 
